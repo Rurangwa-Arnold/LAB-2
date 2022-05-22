2fb39fc8579d3ec51c3dd214013f4f627e557ce8
Rurangwa-Arnold <81275837+Rurangwa-Arnold@users.noreply.github.com>
2022-05-22 02:32:50 +0200

    calculator.php

---
+++ calculator.php
@@ -0,0 +1,51 @@
+<?php
+function sum(float $num1,float $num2): float { 
+    return  $num1 + $num2;
+}
+function diff(float $num1,float $num2): float {
+    return $num1 - $num2;
+
+}
+function div(float $num1,float $num2): float {
+    return $num1 / $num2;
+}
+function multiply(float $num1,float $num2): float {
+    return $num1 * $num2;
+}
+
+echo sum (10,2)."<br/>";
+echo diff (10,2). "<br/>"; ;
+echo div (10,2). "<br/>";
+echo multiply (10,2). "<br/>";
+
+function calculate1(float $num1,string $opp,float $num2): float{
+    if(!($opp == '+' or $opp == '-' or $opp == '/' or $opp == '*')){
+        return 0;
+    }
+    switch ($opp) {
+        case '+': return sum($num1,$num2);
+        case '-': return diff($num1,$num2);
+        case '*': return multiply($num1,$num2);
+        case '/': return div ($num1,$num2);
+    }
+}
+echo calculate1(1,'+',2)."<br/>";
+echo calculate1(1,'-',2)."<br/>";
+echo calculate1(1 ,'*',2)."<br/>";
+echo calculate1 ( 1 ,'/',2)."<br/>";
+function calculate2(float $num1,string $opp,float $num2): float{
+    if (!in_array($opp, ['+','-','*','/'])) {
+        return 0;
+    }
+    $function_name =['+' => 'sum',
+'-' => 'diff',
+'*' => 'multply',
+'/' => 'div' ];
+    return $function_name [$opp]($num1, $num2);
+}
+
+
+
+
+
+

35e86a75c0a1f8867907c79ed38a9e182903404e
Rurangwa-Arnold <81275837+Rurangwa-Arnold@users.noreply.github.com>
2022-05-22 03:11:30 +0200

    calculator.php

--- calculator.php
+++ calculator.php
@@ -33,16 +33,6 @@ echo calculate1(1,'+',2)."<br/>";
 echo calculate1(1,'-',2)."<br/>";
 echo calculate1(1 ,'*',2)."<br/>";
 echo calculate1 ( 1 ,'/',2)."<br/>";
-function calculate2(float $num1,string $opp,float $num2): float{
-    if (!in_array($opp, ['+','-','*','/'])) {
-        return 0;
-    }
-    $function_name =['+' => 'sum',
-'-' => 'diff',
-'*' => 'multply',
-'/' => 'div' ];
-    return $function_name [$opp]($num1, $num2);
-}
 
 
 

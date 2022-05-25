da70864b298c11b437ed33d8145c16fe7f51a3b3
Rurangwa-Arnold <81275837+Rurangwa-Arnold@users.noreply.github.com>
2022-05-25 13:31:22 +0200

    task.php

--- task.php
+++ task.php
@@ -1,1 +1,12 @@
-t
+<?php
+function test(string $text):void {
+    echo($text);
+}
+test('paragraph');
+function call(int $count=1):void {
+    if ($count == 0) {
+        return;
+    }
+    test('Big World')
+}
+call(5);

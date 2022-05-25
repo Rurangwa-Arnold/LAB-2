4eba61a654e955a497eabcf87ccbbd37c87b00c8
Rurangwa-Arnold <81275837+Rurangwa-Arnold@users.noreply.github.com>
2022-05-25 12:18:39 +0200

    Super-calculator.php

---
+++ Super-calculator.php
@@ -0,0 +1,14 @@
+<?php
+function recurrence(int $sum,$mult): int {
+    return recurrence;
+    }
+function sum (int $num1, $num2): int {
+
+return $num1 + $num2;
+ }
+ function mult (int $num1,$num2): int {
+     return $num1 * $num2;
+ }
+ echo sum (1,2);
+ echo mult (3,3);
+ 
